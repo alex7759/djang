008fe8a85de253152a228b81f1475f550b0c1e2c
Администратор <al-shalaev@yandex.ru>
2022-06-06 16:06:20 +0300

    vacancy

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import City, Languare
+from .models import City, Languare, Vacancy
 
 admin.site.register(City)
 admin.site.register(Languare)
+admin.site.register(Vacancy)
 # Register your models here.
--- src/scraping/models.py
+++ src/scraping/models.py
@@ -36,3 +36,19 @@ class Languare(models.Model):
         if not self.slug:
             self.slug = from_cirillic_to_eng(str(self.name))
         super().save(*args, **kwargs)
+
+class Vacancy(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заголовок вакансии')
+    company = models.CharField(max_length=250, verbose_name='Компания')
+    description = models.TextField(verbose_name='Описание вакансии')
+    city = models.ForeignKey('City', on_delete=models.CASCADE, verbose_name='Город')
+    languare = models.ForeignKey('Languare', on_delete=models.CASCADE, verbose_name='Язык програмирования')
+    timestamp = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Вакансия'
+        verbose_name_plural = 'Вакансии'
+
+    def __str__(self):
+        return self.title
